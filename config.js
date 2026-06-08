effad7b825463acdad7d048b4abd73ec4efb6b36
Klaus Kirnbauer <klaus@messnotebook.htlpinkafeld.at>
2026-06-08 10:16:52 +0200

    Add config.js

--- config.js
+++ config.js
@@ -17,16 +17,10 @@ const configData = {
       ]
     },
     {
-      "name": "Speisen & Mehlspeisen",
+      "name": "Kaffee & Mehlspeisen",
       "id": "speisen",
-      "label": "Speisen & Mehlspeisen",
+      "label": "Kaffee & Mehlspeisen",
       "artikel": [
-        {"name": "Schnitzelsemmel (A,C,F,G)", "preis": 5.00},
-        {"name": "Toastlangos (A,C,G)", "preis": 6.00},
-        {"name": "Frankfurter mit Gebäck (A,F,G)", "preis": 4.00},
-        {"name": "Puszta mit Gebäck (A,F,G,L,M)", "preis": 5.00},
-        {"name": "Gemüseschnitzelsemmel (A,C,F,G,L)", "preis": 5.00},
-        {"name": "Semmel (A,F,G)", "preis": 1.00},
         {"name": "Kaffee", "preis": 2.00},
         {"name": "Schulfest: Mehlspeisenbox", "preis": 5.00},
         {"name": "Kindergartenfest: Stück Mehlspeise", "preis": 2.00}
